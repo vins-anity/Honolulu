4ad7af8d6fabdd0449cd91b2b20e7d90f0536a7b
Jhonn Vincent Arcipe <freestyle2.0313@gmail.com>
2026-01-13 04:14:18 +0800

    fckin relative path

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { vValidator } from "@hono/valibot-validator";
-import { HelloWorldSchema } from "shared";
+import { HelloWorldSchema } from "../../shared/src";
 import { db } from "./db";
 import { posts } from "./db/schema";
 
