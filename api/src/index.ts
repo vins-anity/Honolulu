5fc30258d7af0adc3d4f347dd80333b811a64607
Jhonn Vincent Arcipe <freestyle2.0313@gmail.com>
2026-01-13 04:18:14 +0800

    test 2

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { vValidator } from "@hono/valibot-validator";
-import { HelloWorldSchema } from "../../shared/dist";
+import { HelloWorldSchema } from "shared";
 import { db } from "./db";
 import { posts } from "./db/schema";
 
